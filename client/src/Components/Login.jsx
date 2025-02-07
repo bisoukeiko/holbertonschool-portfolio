00d4b2a9bff08a469c1bb31322eea3d9a8e3d242
bisoukeiko <87bisou@gmail.com>
2025-02-07 22:12:34 +0100

    fix code: add login and logout function

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -2,29 +2,65 @@ import React from 'react';
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
 
 function Login() {
 
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  function handleLogout () {
+  async function handleLoginSuccess(credentialResponse) {
+    try {
+      const googleInfo = jwtDecode(credentialResponse.credential);
+      console.log(googleInfo);
+
+      setIsLoggedIn(true);
+
+      // ユーザーがDBに存在するか確認
+      // check User exist in detabase
+      const userExist = await axios.get('http://localhost:5000/userSelect', {params: { sub: googleInfo.sub }});
+      console.log(userExist);
+      if (userExist.data.length > 0) {
+        console.log('user exist');
+        return;
+      } else {
+        await axios.post('http://localhost:5000/userInsert', {
+          gSub: googleInfo.sub,
+          gName: googleInfo.name,
+          gEmail: googleInfo.email
+        })
+      }
+
+      navigate('/home');
+    } catch (error) {
+      console.error('Error login process:', error);
+    }
+  }
+
+  function handleLogout() {
     googleLogout();
+    setIsLoggedIn(false);
+    console.log('logout');
   }
 
   return (
-    <div>
-      <h2>Google ログイン</h2>
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          console.log('Login Success:', credentialResponse);
-          console.log(jwtDecode(credentialResponse.credential));
-          navigate('/home');
-        }}
-        onError={() => {
-          console.log('Login Failed');
-        }}
-      />
-
+    <div className="d-flex justify-content-center">
+      {isLoggedIn ? (
+        <button className='btn btn-danger' onClick={handleLogout}>
+          Logout
+        </button>
+      ) : (
+        <div className="w-auto">
+          <GoogleLogin
+            onSuccess={handleLoginSuccess}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 }

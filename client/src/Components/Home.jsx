9d657b2042d5ef6f4a2c8e39b5baa3c540485d2d
bisoukeiko <87bisou@gmail.com>
2025-02-07 22:15:28 +0100

    fix code: add login, user link

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="grid gap-3">
+      <div className="p-2">Home</div>
+      <div className="p-2"><Link to={`/`} className='btn btn-sm btn-outline-dark'>Login</Link></div>
+      <div className="p-2"><Link to={`/user/`} className='btn btn-sm btn-outline-dark'>User</Link></div>
+    </div>
   )
 }
 

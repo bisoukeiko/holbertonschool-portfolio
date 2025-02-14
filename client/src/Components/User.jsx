6373a78210f2c767baca256f1770b3eb2c0b5e1e
bisoukeiko <87bisou@gmail.com>
2025-02-14 10:50:02 +0100

    fix code: design changes

--- client/src/Components/User.jsx
+++ client/src/Components/User.jsx
@@ -49,7 +49,7 @@ function User() {
     axios.put(`http://localhost:5000/user/update`, userInfo)
     .then((res) => {
       console.log("User updated successfully:", res.data);
-      alert("User updated successfully!");
+      // alert("User updated successfully!");
     })
     .catch((err) => console.error("Error updating user:", err));
     
@@ -58,40 +58,47 @@ function User() {
 
 
   return (
-    <div className='d-flex vh-100 justify-content-center align-items-center'>
+    <div className='d-flex vh-100 flex-column align-items-center'>
+      <h2>User Info</h2>
       <div className='w-25 bg-white rounded p-3 '>
           <form onSubmit={isEdit ? handleUpdate : handleEdit}>
-              <h2>User Info</h2>
-              <div className='mb-2'>
-                  <label htmlFor='user'>user name:</label>
-                  {isEdit ? (
-                      <input id='userName' type='text' placeholder='Enter user name' className='form-control fs-5' value={userInfo.userName}
-                      onChange={e => setUserInfo({...userInfo, userName: e.target.value})}/>
-                  ) : (
-                     <p className='fs-5'>{userInfo.userName}</p>
-                  )}
+
+              <div className='card d-flex flex-column h-100'>
+                <div className='card-header'>
+                    <label htmlFor='userName'>user name:</label>
+                    {isEdit ? (
+                        <input id='userName' type='text' placeholder='Enter user name' className='form-control fs-5' value={userInfo.userName}
+                        onChange={e => setUserInfo({...userInfo, userName: e.target.value})}/>
+                    ) : (
+                      <p className='fs-5'>{userInfo.userName}</p>
+                    )}
+                </div>
+                <div className='card-body flex-grow-1'>
+                  <div className='mb-1'>
+                      <label htmlFor='user'>e-mail:</label>
+                      {isEdit ? (
+                        <input id='userEmail' type='text' placeholder='Enter e-mail' className='form-control fs-5' value={userInfo.userEmail}
+                        onChange={e => setUserInfo({...userInfo, userEmail: e.target.value})}/>
+                      ) : (
+                        <p className='fs-5'>{userInfo.userEmail}</p>
+                      )}
+                  </div>
+                  <div className='mb-1'>
+                      <label htmlFor='user'>phone:</label>
+                      {isEdit ? (
+                        <input id='userPhone' type='text' placeholder='Enter phone number' className='form-control fs-5' value={userInfo.userPhone}
+                        onChange={e => setUserInfo({...userInfo, userPhone: e.target.value})}/>
+                      ) : (
+                        <p className='fs-5'>{userInfo.userPhone}</p>
+                      )}
+                  </div>
+                  <div className='d-flex justify-content-end'>
+                    <button className='btn btn-outline-primary btn-sm'>
+                      {isEdit ? 'Update' : 'Edit'}
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className='mb-2'>
-                  <label htmlFor='user'>e-mail:</label>
-                  {isEdit ? (
-                    <input id='userEmail' type='text' placeholder='Enter e-mail' className='form-control fs-5' value={userInfo.userEmail}
-                    onChange={e => setUserInfo({...userInfo, userEmail: e.target.value})}/>
-                  ) : (
-                    <p className='fs-5'>{userInfo.userEmail}</p>
-                  )}
-              </div>
-              <div className='mb-2'>
-                  <label htmlFor='user'>phone:</label>
-                  {isEdit ? (
-                    <input id='userPhone' type='text' placeholder='Enter phone number' className='form-control fs-5' value={userInfo.userPhone}
-                    onChange={e => setUserInfo({...userInfo, userPhone: e.target.value})}/>
-                  ) : (
-                    <p className='fs-5'>{userInfo.userPhone}</p>
-                  )}
-              </div>
-              <button className='btn btn-outline-info '>
-                {isEdit ? 'Update' : 'Edit'}
-              </button>
           </form>
       </div>
     </div>

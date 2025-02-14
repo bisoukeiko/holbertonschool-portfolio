b915e0a2e1db4d5f9d7c19b45a4ba4939ad4e3a8
bisoukeiko <87bisou@gmail.com>
2025-02-14 11:41:46 +0100

    fix code: add childRoutes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import db from './db.js';
 import todoRoutes from './routes/todoRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import childRoutes from './routes/childRoutes.js';
 
 
 const app = express();
@@ -12,7 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/user', userRoutes);
-// app.use('/api/todo', todoRoutes);
+app.use('/child', childRoutes);
+app.use('/todo', todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);

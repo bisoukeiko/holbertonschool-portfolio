69f0d18e1617c09e1e42de149bef1e52f22f428b
bisoukeiko <87bisou@gmail.com>
2025-02-16 11:12:31 +0100

    fix code: modify layout

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,23 +1,27 @@
 import { Link } from 'react-router-dom';
 import { useUser } from './UserContext';
-import User from "./User";
-import Child from "./Child";
-import Todo from "./Todo";
+import User from './User';
+import Child from './Child';
+import Party from './Party';
+import Todo from './Todo';
 
 function Home() {
   const { userId } = useUser();
 
   return (
-    <div>
-      <h1>Home</h1>
-      {userId ? (
-        <p>Welcome, user with ID: {userId}</p>
-      ) : (
-        <p>Please log in to access the content.</p>  // ログインしていない場合のメッセージ
-      )}
+    <div className='container mt-4'> {/* Bootstrapのコンテナを追加 */}
 
-      <User />
-      <Child />
+      {/* 横並びにするためのrowとcolを追加 */}
+      <div className='row justify-content-center'>
+        <div className='col-md-4'> {/* 画面幅の半分を使用 */}
+          <User />
+        </div>
+        <div className='col-md-4'> {/* 画面幅の半分を使用 */}
+          <Child />
+        </div>
+      </div>
+
+      {/* <Party /> */}
       {/* <Todo /> */}
     </div>
   )

427c555bc745a9c8b5de5b5f351e3c15c591162d
bisoukeiko <87bisou@gmail.com>
2025-02-20 11:16:32 +0100

    first commit

---
+++ server/validators/TodoValidator.js
@@ -0,0 +1,28 @@
+class TodoValidator {
+    constructor(task) {
+        this.todoTask = task;
+        this.errors = [];
+    }
+
+    validateTodoTask() {
+        if (!this.todoTask || this.todoTask.trim() === '') {
+            this.errors.push('Task is required.')
+        }
+
+        if (this.todoTask.length > 50 ) {
+            this.errors.push('Task must be less than 50 lettres.')
+        } 
+    }
+
+
+    validate() {
+        this.validateTodoTask();
+        return this.errors.length === 0;
+    }
+
+    getErrors() {
+        return this.errors;
+    }
+}
+
+export default TodoValidator;

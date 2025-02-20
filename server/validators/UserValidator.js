61a811a5c50c63aa360f02a209ce886d5e24085f
bisoukeiko <87bisou@gmail.com>
2025-02-20 10:22:47 +0100

    first commit

---
+++ server/validators/UserValidator.js
@@ -0,0 +1,48 @@
+class UserValidator {
+    constructor(userName, userEmail, userPhone) {
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPhone = userPhone;
+        this.errors = [];
+    }
+
+    validateUserName() {
+        if (!this.userName || this.userName.trim() === '') {
+            this.errors.push('user name is required.')
+        }
+    }
+
+    validateUserEmail() {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!this.userEmail || this.userEmail.trim() === '') {
+            this.errors.push('E-mail is reqired.')
+        } else if (!emailRegex.test(this.userEmail)) {
+            this.errors.push('Please enter a valid email address.')
+        }
+    }
+
+    validateUserPhone() {
+        const phoneRegex = /^\d{10}$/;
+
+        // remove -, space
+        const phoneReplace = this.userPhone?.replace(/[-\s]/g, '');
+
+        if (this.userPhone && !phoneRegex.test(phoneReplace)) {
+            this.errors.push('Please enter a valid telephone numbre.');
+        }
+    }
+
+    validate() {
+        this.validateUserName();
+        this.validateUserEmail();
+        this.validateUserPhone();
+
+        return this.errors.length === 0;
+    }
+
+    getErrors() {
+        return this.errors;
+    }
+}
+
+export default UserValidator;

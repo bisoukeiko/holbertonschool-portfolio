a9781289c5a9cc0cfad806d375b86dbe089ac1f7
bisoukeiko <87bisou@gmail.com>
2025-03-07 10:10:41 +0100

    fix code: modify google login, get token

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,54 +1,65 @@
-import React from 'react';
-import { GoogleLogin, googleLogout } from '@react-oauth/google';
-import { jwtDecode } from 'jwt-decode';
+import React, { useState } from 'react';
+import { googleLogout, GoogleOAuthProvider, useGoogleLogin  } from '@react-oauth/google';
+// import { GoogleLogin, googleLogout, GoogleOAuthProvider, useGoogleLogin  } from '@react-oauth/google';
+// import { jwtDecode } from 'jwt-decode';
 import { Link, useNavigate } from 'react-router-dom';
-import { useEffect, useState } from "react";
 import axios from 'axios';
 import {useUser} from './UserContext'
 
 
 function Header() {
-  const { userId, login, logout } = useUser();
+
+  const { userId, login, logout, accessToken } = useUser();
 
   const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  async function handleLoginSuccess(credentialResponse) {
-    try {
-      const googleInfo = jwtDecode(credentialResponse.credential);
-      // console.log(googleInfo);
-
-      // ユーザーがDBに存在するか確認
-      // check User exist in detabase
-      let userExist = await axios.get('http://localhost:5000/user/select', {params: { sub: googleInfo.sub }});
-
-      if (userExist.data.length === 0) {
-        await axios.post('http://localhost:5000/user/insert', {
-          gSub: googleInfo.sub,
-          gName: googleInfo.name,
-          gEmail: googleInfo.email
-        });
-
-        userExist = await axios.get('http://localhost:5000/user/select', {params: { sub: googleInfo.sub }});
-      }
-
-      login(userExist.data[0].id_user);  // コンテキストにユーザーIDを保存
-      // console.log('User ID:', userExist.data[0].id_user); 
-
-      navigate('/');
-
-    } catch (error) {
-      console.error('Error login process:', error);
-    }
-  }
 
   function handleLogout() {
     googleLogout();
 
     logout(); //UseContextからインポート 
     console.log('logout');
+    localStorage.clear();
     navigate('/');
   }
+
+  const googlelogin = useGoogleLogin({
+    onSuccess: (codeResponse) => responseGoogle(codeResponse),
+    flow: "auth-code",
+    scope: "email profile openid https://www.googleapis.com/auth/drive.file",
+  });
+
+  const responseGoogle = async (codeResponse) => {
+    try {
+      console.log("Google codeResponse:", JSON.stringify(codeResponse));
+  
+      const response = await axios.post(`http://localhost:5000/auth/google`, { code: codeResponse.code });
+  
+      const googleInfo = response.data.user;
+      const accessToken = response.data.access_token;
+      // const refreshToken = response.data.refresh_token;
+
+      let userExist = await axios.get('http://localhost:5000/user/select', {
+        params: { sub: googleInfo.id }
+      });
+  
+      if (userExist.data.length === 0) {
+        await axios.post('http://localhost:5000/user/insert', {
+          gSub: googleInfo.id,
+          gName: googleInfo.given_name,
+          gEmail: googleInfo.email
+        });
+  
+        userExist = await axios.get('http://localhost:5000/user/select', {
+          params: { sub: googleInfo.id }
+        });
+      }
+  
+      login(userExist.data[0].id_user, accessToken);
+  
+    } catch (error) {
+      console.error('Error login:', error);
+    }
+  };
 
   return (
     <header className="bg-white shadow-sm">
@@ -73,28 +84,45 @@ function Header() {
           </li>
         </ul>
 
-        {/* Googleログイン or ログアウト */}
+        {/* Google login logout */}
         <div>
-          {userId ? (
-            <span
-              className="text-danger fw-bold cursor-pointer border rounded p-2 ps-4 pe-4"
-              style={{ cursor: "pointer" }}
-              onClick={handleLogout}
-            >
-              Logout
-            </span>
-          ) : (
-            <div style={{ width: "200px" }}>
-              <GoogleLogin
-                onSuccess={handleLoginSuccess}
-                onError={() => console.log('Login Failed')}
-              />
-            </div>
-          )}
+            <GoogleOAuthProvider clientId={''}>
+                {!userId ? (
+                  <button className='btn btn-light d-flex align-items-center shadow-sm border rounded' onClick={() => googlelogin()}>
+                      <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Logo" class="me-2" width="20" height="20"/>
+                      <span className="">Sign in with Google</span>
+                  </button>
+                ) : (
+                  <button className='btn btn-light d-flex align-items-center shadow-sm border rounded' onClick={handleLogout}>
+                      <img src="https://img.icons8.com/color/48/000000/google-logo.png" alt="Google Logo" class="me-2" width="20" height="20"/>
+                      <span className='text-danger'>Logout</span>
+                  </button>
+                )}
+            </GoogleOAuthProvider>
         </div>
       </nav>
     </header>
   );
 }
+
+
+// return (
+  // <GoogleOAuthProvider clientId={''}>
+  //   {!isLoggedIn ? (
+  //     <button onClick={() => googlelogin()}>Login</button>
+  //   ) : (
+  //     <div>
+  //       <p>Id: {id}</p>
+  //       <p>Email: {email}</p>
+  //       <p>GivenName: {givenName}</p>
+  //       <p>AccessToken2: {accessToken}</p>
+  //       <p>RefreshToken: {refreshToken}</p>
+  //       <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+  //     </div>
+  //   )}
+  // </GoogleOAuthProvider>
+// );
+// }
+
 
 export default Header;

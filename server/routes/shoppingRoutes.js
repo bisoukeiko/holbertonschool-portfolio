2a68a717c670303a762feb79a1c95aaa554c7ee1
bisoukeiko <87bisou@gmail.com>
2025-03-12 12:26:50 +0100

    fix code: add addInitialItems route

--- server/routes/shoppingRoutes.js
+++ server/routes/shoppingRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getShopping, addShopping, updateShopping, updateShoppingFlag, deleteShopping, deleteShoppingByParty } from '../controllers/shoppingController.js';
+import { getShopping, addShopping, updateShopping, updateShoppingFlag, deleteShopping, deleteShoppingByParty, addInitialItems } from '../controllers/shoppingController.js';
 
 const router = express.Router();
 
 router.get('/select', getShopping);
 router.post('/insert', addShopping);
+router.post('/addInitialItems', addInitialItems);
 router.put('/update/:id', updateShopping);
 router.put('/updateFlag/:id', updateShoppingFlag);
 router.delete('/delete/:id', deleteShopping);

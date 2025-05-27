dc7e3f2f52086d5f17fc74ee894904faf0de8ab5
bisoukeiko <87bisou@gmail.com>
2025-05-27 19:01:55 +0200

    fix code: add and fix Home, Party, CreateCard, rsvp by changing folder structure

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useUser } from '../Components/UserContext';
+import Child from '../Components/Child';
+import home_3 from '../assets/home/home_3.jpg';
+
+
+function Home() {
+  const { userId } = useUser();
+
+  return (
+    <div className='container mt-4'>
+
+    <div className='row text-center g-0'>
+      <div className='col'>
+        {!userId && (
+          <img src={home_3} className="img-fluid w-100" alt="" />
+        )}
+      </div>
+    </div>
+    <div className='row g-0'>
+
+        {/* <div className='col-md-4'>
+        {userId && (
+          <User />
+        )}
+        </div> */}
+        <div className='col'>
+        {userId && (
+          <Child />
+        )}
+        </div>
+
+      </div>
+    </div>
+
+  )
+}
+
+export default Home
---
+++ client/src/Pages/Rsvp.jsx
@@ -0,0 +1,256 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import {Dropdown } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+
+function Rsvp() {
+    const { partyId } = useParams();
+    const navigate = useNavigate();
+
+    const [errValidation, setErrValidation] = useState([]);
+
+    const [guestList, setGuestList] = useState([]);
+    const [partyInfo, setPartyInfo] = useState({});
+
+    const [guestValue, setGuestValue] = useState({
+        idGuest: '',
+        guestName: '',
+        guestRelation: '',
+        guestAllergy: '',
+        otherInfo: '',
+        parentPhone: '',
+        parentEmail: '',
+        idParty: '',
+        fgAttend: ''
+    });
+
+    useEffect(() => {
+      if (partyId) {
+          axios.get(`http://localhost:5000/guest/select`, {params: { partyId: partyId }})
+              .then(res => {
+                  console.log(res.data);
+                  setGuestList(res.data);
+              })
+              .catch(err => console.log(err));
+
+          axios.get(`http://localhost:5000/party/select`, {params: { partyId: partyId }})
+          .then(res => {
+              // console.log('party: ', res.data[0]);
+              setPartyInfo(res.data[0]);
+          })
+          .catch(err => console.log(err));
+       }
+    }, [partyId]);
+
+
+    const getOrdinalSuffix = (num) => {
+      if (num === 1) return 'st';
+      if (num === 2) return 'nd';
+      if (num === 3) return 'rd';
+      return 'th';
+    };
+
+
+    const handleSetGuest = (guestData) => {
+      setGuestValue({
+        idGuest: guestData.idGuest || '',
+        guestName: guestData.guestName || '',
+        guestRelation: guestData.guestRelation || '',
+        guestAllergy: guestData.guestAllergy || '',
+        otherInfo: guestData.otherInfo || '',
+        parentPhone: guestData.parentPhone || '',
+        parentEmail: guestData.parentEmail || '',
+        idParty: guestData.idParty || '',
+        fgAttend: guestData.fgAttend ?? ''
+      });
+    }
+
+    const handleChange = (field, value) => {
+      setGuestValue({ ...guestValue, [field]: value});
+    };
+
+
+    const handleSend = (event) => {
+      event.preventDefault();
+      setErrValidation([])
+
+      const updatedValues = { ...guestValue };
+
+      let errors = [];
+
+      if (!updatedValues.fgAttend) {
+        errors.push("Attendance confirmation is required. Please select Yes or No.");
+      }
+
+      if (!updatedValues.parentPhone) {
+        errors.push("Parent's phone number is required.");
+      }
+
+      if (errors.length > 0) {
+        setErrValidation(errors.join('\n'));
+        return;
+      }
+
+      // console.log('updatedValues', updatedValues);
+      axios.put(`http://localhost:5000/guest/updateRsvp/${updatedValues.idGuest}`, {...updatedValues, 'partyId': partyId})
+      .then(res => {
+          alert("Your response registered successfully!");
+          setErrValidation([])
+          navigate('/');
+      })
+      .catch((err) => {
+          if (err.response && err.response.data.errors) {
+            setErrValidation(err.response.data.errors.join('\n'));
+          } else {
+            console.error('Error updating guest:', err);
+          }
+      });
+    }
+
+  return (
+    <div className='bg-info-subtle'>
+        <div className='container py-4'>
+        <div className='d-flex flex-column justify-content-center align-items-center'>
+
+          <h2 className='text-center mt-5 mb-5'>🎂 RSVP for the Birthday Party🎈</h2>
+          <div className='text-center mb-5'>
+              Please let us know if your child can make it to the birthday party!
+          </div>
+
+
+              <div className='card shadow-lg w-75 p-4 mb-5'>
+                <h3 className='text-center mb-3'>
+                  {partyInfo.childName}'s {partyInfo.childYears}{getOrdinalSuffix(partyInfo.childYears)} Birthday Party 🎉
+                </h3>
+
+                <div className='d-flex flex-column align-items-center gap-3'>
+                  <div className='d-flex flex-wrap justify-content-center gap-5'>
+                    <div className='text-center'>
+                      <h5 className='text-primary'>📅 Date</h5>
+                      <p>{partyInfo.partyDate2}</p>
+                    </div>
+                    <div className='text-center'>
+                      <h5 className='text-primary'>⏰ Time</h5>
+                      <p>{partyInfo.partyTimeFrom} - {partyInfo.partyTimeTo}</p>
+                    </div>
+                    <div className='text-center'>
+                      <h5 className='text-primary'>📍 Location</h5>
+                      <p className='mb-0'>{partyInfo.partyPlace}</p>
+                      <p className='mb-0'>{partyInfo.partyPlace2 || ''}</p>
+                      <p>{partyInfo.partyPlace3 || ''}</p>
+                    </div>
+                  </div>
+
+                  <div className='text-center'>
+                    <h5 className='text-primary'>📞 Contact</h5>
+                    <p>{partyInfo.partyContact1} {partyInfo.partyContact2 && `/ ${partyInfo.partyContact2}`}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className='card shadow-l w-75 p-5 mb-5'>
+
+                <div className='d-flex flex-column justify-content-center align-items-center p-4'>
+                    {/* dropdown child name */}
+                    <Dropdown>
+                        <Dropdown.Toggle variant='light' id='dropdown-basic'className='fs-6 mb-4 border' style={{ width: '200px' }}>
+                        {guestValue.guestName
+                              ? `${guestValue.guestName} (${guestValue.guestRelation || ''})` 
+                              : 'Select guest name'}
+                        </Dropdown.Toggle>
+              
+                        <Dropdown.Menu  style={{ width: '200px' }}>
+                            {guestList.map((guest) => (
+                                <Dropdown.Item 
+                                    key={guest.idGuest} 
+                                    onClick={() => {handleSetGuest(guest)}}>
+                                    {/* onClick={() => {setGuestValue(guest)}}> */}
+                                    {`${guest.guestName} (${guest.guestRelation || ''})`}
+                                </Dropdown.Item>
+                            ))}
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <div className=' mt-4 mb-2'>
+                        Can he/she come?
+                    </div>
+
+                    <div className='d-flex mb-5'>
+                        <div className='form-check me-5'>
+                          <input
+                            className='form-check-input'
+                            type='radio'
+                            name={`fgAttend_${guestValue.idGuest}`}
+                            id='fgAttend1'
+                            checked={guestValue.fgAttend === '1'}
+                            onChange={() => handleChange('fgAttend', '1')}
+                          />
+                          <label className='form-check-label' htmlFor='fgAttend1'>
+                            Yes
+                          </label>
+                        </div>
+
+                        <div className='form-check'>
+                          <input
+                            className='form-check-input'
+                            type='radio'
+                            name={`fgAttend_${guestValue.idGuest}`}
+                            id='fgAttend2'
+                            checked={guestValue.fgAttend === '2'}
+                            onChange={() => handleChange('fgAttend', '2')}
+                          />
+                          <label className='form-check-label' htmlFor='fgAttend2'>
+                            No
+                          </label>
+                        </div>
+                    </div>  
+
+                    <div className='' style={{ width: '40%' }}>
+
+                        <label htmlFor='guestAllergy' className='ms-2'>Allergy</label>
+                        <input id='guestAllergy' type='text' placeholder='' className='form-control mb-5' value={guestValue.guestAllergy}
+                            onChange={event => handleChange('guestAllergy', event.target.value)}
+                        />
+
+                        <label htmlFor='parentPhone' className='ms-2'>Parents phone number *</label>
+                        <input id='parentPhone' type='text' placeholder='' className='form-control mb-5' value={guestValue.parentPhone}
+                            onChange={event => handleChange('parentPhone', event.target.value)}
+                        />
+
+                        <label htmlFor='parentEmail' className='ms-2'>Parents E-mail</label>
+                        <input id='parentEmail' type='text' placeholder='' className='form-control mb-5' value={guestValue.parentEmail}
+                            onChange={event => handleChange('parentEmail', event.target.value)}
+                        />
+
+                        <label htmlFor='otherInfo' className='ms-2'>Other Information</label>
+                        <textarea id='otherInfo' type='text' placeholder='' className='form-control mb-5' value={guestValue.otherInfo}
+                            onChange={event => handleChange('otherInfo', event.target.value)}>
+                        </textarea>
+
+                    </div>
+
+                    {/* error message */}
+                    <div>
+                        {errValidation && (
+                            <div className='text-danger mb-4' style={{ whiteSpace: 'pre-wrap' }}>
+                                {errValidation}
+                            </div>
+                        )}
+                    </div>
+
+                    <button class='btn btn-primary' type='submit' onClick={handleSend}>
+                        Send
+                    </button>
+
+                </div>
+              </div>
+        </div>
+      </div>
+  </div>
+
+  )
+}
+
+export default Rsvp
